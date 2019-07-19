b162a07cc08b0a11b1b898a8ea2f359e9d7e1312
Solleter <Solleter@outlook.com>
2019-07-19 19:42:19 +0800

    Init Project

---
+++ Assets/Libs/Test/FlatBufferTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FlatBuffers;
+
+public class FlatBufferTest : MonoBehaviour
+{
+
+    private void CreateLoginMsg()
+    {
+        string accountID = "TestAccountID";
+        string deviceUUID = "Test Device UUID";
+        string token = "Test Token";
+
+
+        FlatBufferBuilder builder = new FlatBufferBuilder(1);
+        StringOffset accountValue = builder.CreateString(accountID);
+        StringOffset deviceUUIDValue = builder.CreateString(deviceUUID);
+        StringOffset tokenValue = builder.CreateString(token);
+        var offset = CSLogin.CreateCSLogin(builder, accountValue, deviceUUIDValue, tokenValue);
+        builder.Finish(offset.Value);
+        byte[] data = builder.SizedByteArray();
+
+        // Send data to network
+    }
+}
---
+++ Tools/gen_client.bat
@@ -0,0 +1,1 @@
+.\flatc.exe --csharp -o ..\Assets\Scripts\GeneratedMsg\ ..\FlatBufferMessages\Login.fbs ..\FlatBufferMessages\Role.fbs ..\FlatBufferMessages\Common.fbs --gen-onefile
