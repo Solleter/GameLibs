a8809c1bb1dce62e2db2a84e77f514d54235f7ad
Solleter <Solleter@outlook.com>
2019-08-01 23:05:43 +0800

    添加flatbuffers测试

---
+++ Assets/FlatBuffer/ReadFlatBufferData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using FlatBuffers;
+
+public class ReadFlatBufferData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string filePath = Application.dataPath + "/Config/fbmonster.bytes";
+        byte[] buffer = File.ReadAllBytes(filePath);
+
+        ByteBuffer bb = new ByteBuffer(buffer);
+        FBMonster fbMonster = FBMonster.GetRootAsFBMonster(bb);
+        string name = fbMonster.Name;
+        int hp = fbMonster.Hp;
+        int attack = fbMonster.Attack;
+
+        Debug.LogFormat("Name: {0}, HP: {1}, Attack: {2}", name, hp, attack);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
