31c88771f76fc002f77d3974104827dd3f12c4f3
Solleter <Solleter@outlook.com>
2019-08-06 13:40:16 +0800

    Add joystick

--- Assets/Game/Scripts/AimController.cs
+++ Assets/Game/Scripts/AimController.cs
@@ -6,12 +6,13 @@ public class AimController : MonoBehaviour
     public DefaultGun gun;
     private Vector3 screenCenter;
     private Vector3 normDir;
+    private Quaternion targetQ;
+    private float angle;
 
-    private Transform myTrans;
+    public Transform bodyTrans;
 
     private void Start()
     {
-        myTrans = transform;
         screenCenter = new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0);
     }
 
@@ -27,7 +28,9 @@ public class AimController : MonoBehaviour
     /// </summary>
     private void UpdateAim()
     {
-        myTrans.rotation = Quaternion.LookRotation(normDir);
+        //bodyTrans.rotation =  Quaternion.Lerp(bodyTrans.rotation, targetQ, 0.3f);
+        
+        bodyTrans.rotation = Quaternion.LookRotation(normDir);
     }
 
     /// <summary>
@@ -37,6 +40,9 @@ public class AimController : MonoBehaviour
     {
         Vector3 mousePos = Input.mousePosition;
         normDir = (mousePos - screenCenter).normalized;
+        targetQ = Quaternion.LookRotation(normDir, Vector3.forward);
+        angle = Vector3.Angle(Vector3.up, normDir);
+
     }
 
     private void UpdateShooting()
@@ -54,6 +60,6 @@ public class AimController : MonoBehaviour
     private void OnGUI()
     {
         GUILayout.Label(Screen.width + "  " + Screen.height + "  " + screenCenter);
-        GUILayout.Label("NormDir: " + normDir);
+        GUILayout.Label("NormDir: " + normDir + "  angle: " + angle);
     }
 }
