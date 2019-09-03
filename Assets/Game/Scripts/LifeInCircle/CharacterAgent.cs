d0aa72f52db45bdc08f92b5c9aab618fdd16a1aa
Solleter <Solleter@outlook.com>
2019-09-03 19:25:43 +0800

    Update

---
+++ Assets/Game/Scripts/LifeInCircle/CharacterAgent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LifeInCircle.GameLogic
+{
+    public class CharacterAgent : MonoBehaviour
+    {
+        public float moveSpeed = 1.0f;
+
+        private Transform trans;
+
+        private void Awake()
+        {
+            trans = transform;
+        }
+
+        public void DoMove(Vector3 moveStep)
+        {
+            trans.position += moveStep * moveSpeed * Time.deltaTime;
+        }
+
+        public void DoRotate(Vector3 forward)
+        {
+            this.forward = forward;
+            MakeYForwardDir(forward);
+        }
+
+        private void MakeYForwardDir(Vector3 forward)
+        {
+            Vector3 currPos = trans.position;
+            float dotValue = Vector3.Dot(Vector3.up, forward);
+            float angle = Mathf.Acos(dotValue) * Mathf.Rad2Deg;
+            Vector3 tempDir = Vector3.Cross(Vector3.up, forward);
+            angle *= tempDir.z < 0 ? -1 : 1;
+            //trans.RotateAround(currPos, Vector3.forward, angle);
+           trans.eulerAngles = new Vector3(0, 0, angle);
+            debugAngle = angle;
+        }
+
+        private float debugAngle = 0.0f;
+        private Vector3 forward;
+        private void OnGUI()
+        {
+            GUILayout.Label("Angle: " + debugAngle);
+            GUILayout.Label("Forward: " + forward);
+            GUILayout.Label("UP: " + trans.up);
+        }
+
+    }
+}
---
+++ Assets/Game/Scripts/LifeInCircle/InputController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LifeInCircle.GameLogic
+{
+    public class InputController : MonoBehaviour
+    {
+        public CharacterAgent agent;
+
+        private Vector3 moveVector;    // normalize move vector
+        private Vector3 forwardVector; // normalize forward vector
+
+        private void Update()
+        {
+            ProcessKeyboardInput();
+            agent.DoMove(moveVector);
+            agent.DoRotate(forwardVector);
+        }
+
+        private void ProcessKeyboardInput()
+        {
+            #region Move
+            // Left
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                moveVector.x = -1;
+            }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                if(moveVector.x < 0)
+                {
+                    moveVector.x = 0;
+                }
+            }
+
+            // Right
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                moveVector.x = 1;
+            }
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                if(moveVector.x > 0)
+                {
+                    moveVector.x = 0;
+                }
+            }
+
+            // Up
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                moveVector.y = 1;
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                if(moveVector.y > 0)
+                {
+                    moveVector.y = 0;
+                }
+            }
+
+            // Down
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                moveVector.y = -1;
+            }
+            else if (Input.GetKeyUp(KeyCode.S))
+            {
+                if(moveVector.y < 0)
+                {
+                    moveVector.y = 0;
+                }
+            }
+            #endregion
+
+            #region rotate
+            //Vector3 screenPos = new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0);
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            //forwardVector = (mousePos - screenPos).normalized;
+            #endregion
+
+
+        }
+    }
+}
